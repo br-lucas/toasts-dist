439708e3044c0c0bc14fef5ca94ae400cefb07ca
Lucas <lucas@brokenrubik.co>
2021-09-29 20:47:01 -0300

    Initial commit

---
+++ dist/LazyLoaded-e48965a6.js
@@ -0,0 +1,2 @@
+import{S as s,i as a,s as n,e,a as t,n as o,d as r}from"./main-75794d0c.js";function c(s){let a;return{c(){a=e("section"),a.innerHTML="<h1>Lazy loaded component!</h1>"},m(s,n){t(s,a,n)},p:o,i:o,o:o,d(s){s&&r(a)}}}export default class extends s{constructor(s){super(),a(this,s,null,c,n,{})}}
+//# sourceMappingURL=LazyLoaded-e48965a6.js.map
---
+++ dist/LazyLoaded-ea164384.js
@@ -0,0 +1,2 @@
+import{S as a,i as s,s as n,e,a as t,n as o,d as r}from"./main-35abafac.js";function c(a){let s;return{c(){s=e("section"),s.innerHTML="<h1>Lazy loaded component!</h1>"},m(a,n){t(a,s,n)},p:o,i:o,o:o,d(a){a&&r(s)}}}export default class extends a{constructor(a){super(),s(this,a,null,c,n,{})}}
+//# sourceMappingURL=LazyLoaded-ea164384.js.map
